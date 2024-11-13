bc7072d9d4c9a408030f4aa7aeba9fd4a4b9b691
Rupeshpharande <pharanderupesh@gmail.com>
2024-11-14 01:15:43 +0530

    Add project files.

---
+++ Arraylistdemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraylistdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 30, 40, 50 };
+            ArrayList list1 = new ArrayList();
+            list1.Add(10);
+            list1.Add(20);
+
+            list1.AddRange(arr);
+
+            list1.Insert(1, 60);
+
+          //  list1.Remove(10);
+           // list1.RemoveAt(2);
+
+            list1.RemoveRange(3, 2);
+            // list1.Clear();
+            Console.WriteLine("count ="+list1.Count);
+            Console.WriteLine("Capasity="+list1.Capacity);
+
+            foreach(var i in list1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
